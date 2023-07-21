cd13e2b74906f7391c979250715a32f7caccc2ef
Benedikt Franke <benedikt.franke@mll.com>
2023-07-21 14:28:33 +0200

    Add Laravel rule to convert `collect()` to `new Illuminate\Support\Collection()` (#1)

--- config.php
+++ config.php
@@ -5,10 +5,9 @@ namespace MLL\RectorConfig;
 use Rector\Config\RectorConfig;
 use Rector\Php81\Rector\Array_\FirstClassCallableRector;
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
+use Rector\Transform\Rector\FuncCall\FuncCallToNewRector;
 
-/**
- * Configure rector with MLL rules.
- */
+/** Configure rector with PHP rules. */
 function config(RectorConfig $rectorConfig): void
 {
     /**
@@ -24,6 +23,14 @@ function config(RectorConfig $rectorConfig): void
      * ]);
      */
     $rectorConfig->rule(FinalizeClassesWithoutChildrenRector::class);
-
     $rectorConfig->rule(FirstClassCallableRector::class);
+}
+
+/** Configure rector with Laravel rules. */
+function laravel(RectorConfig $rectorConfig): void
+{
+    config($rectorConfig);
+    $rectorConfig->ruleWithConfiguration(FuncCallToNewRector::class, [
+        'collect' => 'Illuminate\\Support\\Collection',
+    ]);
 }
