7d45d4cfa17920969b4310491ff714580f24fe2b
Benedikt Franke <benedikt.franke@mll.com>
2025-10-16 10:36:47 +0200

    Enforce usage of \Illuminate\Support\Str over \Str

--- config.php
+++ config.php
@@ -3,28 +3,27 @@
 namespace MLL\RectorConfig;
 
 use Rector\Config\RectorConfig;
-use Rector\Php81\Rector\Array_\FirstClassCallableRector;
-use Rector\Transform\Rector\FuncCall\FuncCallToNewRector;
-use Rector\Transform\Rector\StaticCall\StaticCallToNewRector;
-use Rector\Transform\ValueObject\StaticCallToNew;
 
 /** Configure rector with PHP rules. */
 function config(RectorConfig $rectorConfig): void
 {
-    $rectorConfig->rule(FirstClassCallableRector::class);
+    $rectorConfig->rule(\Rector\Php81\Rector\Array_\FirstClassCallableRector::class);
 }
 
 /** Configure rector with Laravel rules. */
 function laravel(RectorConfig $rectorConfig): void
 {
     config($rectorConfig);
-    $rectorConfig->ruleWithConfiguration(FuncCallToNewRector::class, [
+    $rectorConfig->ruleWithConfiguration(\Rector\Transform\Rector\FuncCall\FuncCallToNewRector::class, [
         'collect' => \Illuminate\Support\Collection::class,
     ]);
-    $rectorConfig->ruleWithConfiguration(StaticCallToNewRector::class, [
-        new StaticCallToNew(
+    $rectorConfig->ruleWithConfiguration(\Rector\Transform\Rector\StaticCall\StaticCallToNewRector::class, [
+        new \Rector\Transform\ValueObject\StaticCallToNew(
             class: \Illuminate\Support\Collection::class,
             method: 'make',
         ),
+    ]);
+    $rectorConfig->ruleWithConfiguration(\Rector\Renaming\Rector\Name\RenameClassRector::class, [
+        \Str::class => \Illuminate\Support\Str::class,
     ]);
 }
